c707f2f6171a2543064e729f635275fa1675b1be
Moriarty Link <151455290+MoriartyLink@users.noreply.github.com>
2025-04-29 08:42:26 +0630

    Update urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('add-to-cart/<int:dress_id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/', views.view_cart, name='view_cart'),
     path('remove-from-cart/<int:dress_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('create-checkout-session/', views.create_checkout_session, name='create_checkout_session'),
 ]
